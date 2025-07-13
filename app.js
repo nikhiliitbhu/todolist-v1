74978debb14eede7feb4ee28613a3101c51d68c4
Nikhil <nksingh7000@gmail.com>
2025-07-13 18:18:00 +0530

    changes to push on render

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const today = require(__dirname + "/date.js");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
