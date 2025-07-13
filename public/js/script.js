347776fcacc18b34851edd8b94bb2c6da620f5e1
Nikhil <nksingh7000@gmail.com>
2025-07-13 17:29:38 +0530

    button feature for material and spitiual list

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,7 @@
 let clickedButton = null;
 
+document.getElementById(window.location.pathname).setAttribute("class", "clickedBtn");
+
   // Detect which button was clicked
   document.querySelectorAll('#listId button[type="submit"]').forEach(button => {
     button.addEventListener('click', function(event) {
@@ -18,7 +20,16 @@ let clickedButton = null;
   });
 
 
+  if(myList){
   new Sortable(myList, {
     animation: 150
   });
+    }
+
+  function makeRequest(url) {
+    
+    window.open(window.location.origin + url, "_target");
+           
+    }
+
     
