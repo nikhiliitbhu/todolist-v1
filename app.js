42b89b52abfa9a92e5da259f2e5c8d6f6a6195bd
Nikhil <nksingh7000@gmail.com>
2025-07-22 01:34:37 +0530

    introduce mongodb

--- app.js
+++ app.js
@@ -1,24 +1,42 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+require('dotenv').config();
 const today = require(__dirname + "/date.js");
 
+//Middleware and port
 const app = express();
 const port = process.env.PORT || 3000;
-
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 app.set('view engine','ejs');
 
+//Mongoose connection
+mongoose.connect(process.env.MONGO_URI).then(() => {
+  console.log("MongoDB connected!");
+}).catch((err) => {
+  console.error("MongoDB connection error:", err);
+});
+
+//mongoose db schema definition
+const todoSchema = new mongoose.Schema({
+    item: String
+});
+
+const list = mongoose.model('list1', todoSchema);
+
+
+//Server setup
 app.listen(port, function(){
     console.log("app is up at " + port);
 })
 
-const materialList = [];
+// console.log(materialList);
 const spiritualList = [];
 
-app.get("/", function(req, res){
-
-
+app.get("/", async function(req, res){
+    const materialList = await list.find({},{_id: 0, item: 1});
+    console.log(typeof materialList.item);
     res.render("index", {date: today, listType: "material", list : materialList });
 })
 
@@ -46,7 +64,3 @@ app.post("/submit", function(req, res){
 
 })
 
-app.get("/spiritual", function(req, res){
-    res.render("index", {date: today, listType: "spiritual", list : spiritualList });
-
-})
